f97d11baf82030ab71a199e62dbe18286bb58344
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-04-24 08:47:38 +0200

    Tag 2: Variablen und Datentypen

--- Woche-1/Tag-2/src/Steckbrief.kt
+++ Woche-1/Tag-2/src/Steckbrief.kt
@@ -7,4 +7,13 @@ fun main(){
 
     println("Größe: %.2f\nName: Moh".format(groesse))
 
+    // Andere Datentypen als String einlesen:
+        // .toInt()
+        // .toBoolean()
+        // .toDouble()
+    println("Wie alt sind sie? geben sie in Jahren ein:")
+    var alter: Int = readln().toInt()
+
+    println("Sie sind $alter Jahre alt")
+
 }
