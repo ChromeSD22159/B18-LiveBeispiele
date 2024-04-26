e95261b290442d9415a0a79835859fe55d53ca01
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-04-26 08:25:07 +0200

    Tag 4: String

--- Woche-1/Tag-4/src/StringSlice.kt
+++ Woche-1/Tag-4/src/StringSlice.kt
@@ -1,16 +1,16 @@
 
 fun main(){
 
-
+                        //  0123456
     var beispiel: String = "passwort123456#12351234"
     // slice erlaubt uns den String auszuschneiden
 
     // von .. bis
-    var ausschnitt: String = beispiel.slice(0..14)
+    var ausschnitt: String = beispiel.slice(0..14)  // von 3 .. bis 14
     println(ausschnitt)
 
 
-    ausschnitt = beispiel.substring(5, 10)
+    ausschnitt = beispiel.substring(0, 15)
     println(ausschnitt)
 
 
