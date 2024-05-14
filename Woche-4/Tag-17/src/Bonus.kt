c3e032f29e95946d802d7518df3337d38a6f2f2f
ChromeSD22159 <65664594+ChromeSD22159@users.noreply.github.com>
2024-05-14 12:38:14 +0200

    Race solution

--- Woche-4/Tag-17/src/Bonus.kt
+++ Woche-4/Tag-17/src/Bonus.kt
@@ -1,5 +1,7 @@
 package bonus
 
+import kotlin.random.Random
+
 /**
  *  Die heutige Bonusaufgabe simuliert ein Auto-Rennen.
  *  Es sind zwei Autos gegeben.
@@ -9,6 +11,7 @@ package bonus
  *  sodass der Spielfluss in [game] richtig funktioniert.
  */
 fun main(){
+
     game()
 }
 
@@ -28,62 +31,73 @@ val car2: MutableMap<String, Int> = mutableMapOf(
     "Zeit" to 0
 )
 
+const val maxProgress = 20
 
 
-/**
- *  Diese Funktion wiederholt das Spiel so lange, bis ein Auto gewonnen hat.
- *  Dann druckt sie den Gewinner des Rennens aus.
- *
- *  Hier müsst ihr nichts verändern.
- */
-fun game(){
-    if (raceOver())
+
+fun game() {
+    if (!raceOver()) {
+        drive(car1, "Car1")
+        drive(car2, "Car2")
+        Thread.sleep(500)
+        game()
+    } else {
         declareWinner()
-    else {
-        drive(car1)
-        drive(car2)
-        game()  // Startet die nächste Runde des Spiels
+        Thread.sleep(5000)
+    }
+
+}
+
+fun drive(car: MutableMap<String, Int>, name: String){
+    if (car["Fortschritt"]!! < maxProgress) {
+        val newFortschritt = car["Fortschritt"]!! + randomProgress(car["Tempo"]!!)
+        val newTime = car["Zeit"]!! + 1
+        val newTank = car["Tank"]!! - randomTankReduce(3, 7)
+
+        car["Fortschritt"] = newFortschritt
+        car["Zeit"] = newTime
+        car["Tank"] = newTank
+
+        println("- - -")
+        println("$name:")
+        println("Fortschritt: $newFortschritt")
+        println("Zeit: $newTime")
+        println("Tank: $newTank")
+        println("- - -\n")
+    }
+
+    if (car["Tank"]!! <= 7) {
+        pitStop(car)
+        println("$name macht einen Boxenstopp...")
     }
 }
 
-
-/**
- *  TODO:
- *   Diese Funktion soll einen Spielzug für das übergebene Auto simulieren.
- *   Wenn das Auto im Ziel ist, kann das Auto einfach warten.
- *   Wenn das Auto noch nicht im Ziel ist, soll es fahren.
- *      Dabei macht es einen Fortschritt der zufällig zw. 1 und Speed liegt.
- *      Jedes mal, wenn das Auto fährt, soll die Zeit um 1 erhöht werden,
- *      und der Tank um einen zufälligen Wert zwischen 3..7 reduziert werden.
- *   Sollte der Tank weniger als 7 betragen, muss das Auto einen Pitstop ablegen und kann nicht fahren.
- *      Der Pitstop kostet eine Zeit von 3 und füllt den Tank wieder auf das Maximum auf.
- */
-fun drive(car: MutableMap<String, Int>){
-
+fun randomProgress(max: Int): Int {
+    return Random.nextInt(1,max)
 }
 
-/**
- *  Diese Funktion braucht es nicht zwingend, könnte aber hilfreich sein.
- */
+fun randomTankReduce(from: Int, until: Int): Int {
+    return Random.nextInt(from,until)
+}
+
 fun pitStop(car: MutableMap<String, Int>){
-
+    car["Zeit"] = car["Zeit"]!! - 3
+    car["Tank"] = car["MaxTank"]!!
 }
 
-/** TODO:
-     Diese Funktion soll ermitteln, ob das Rennen zu Ende ist oder nicht.
-     Das Rennen ist zu Ende, wenn beide Fahrer das Ziel erreicht haben (Fortschritt >= 100)
- */
 fun raceOver(): Boolean {
-
-    return false // TODO: Hier muss noch der richtige Wert zurückgegeben werden
+    val raceState = car1["Fortschritt"]!! >= maxProgress && car2["Fortschritt"]!! >= maxProgress
+    return raceState
 }
 
-/**
- * TODO:
- *  Diese Funktion soll auf der Konsole ausgeben, welches Auto gewonnen hat.
- *  Sie soll das nur Tun, wenn das Rennen bereits zu Ende ist [raceOver].
- */
 fun declareWinner(){
-
+    if (car1["Fortschritt"]!! >= maxProgress && car2["Fortschritt"]!! >= maxProgress) {
+        if (car1["Zeit"]!! < car2["Zeit"]!!)
+            println("Auto 1 hat gewonnen!")
+        else if (car2["Zeit"]!! < car1["Zeit"]!!)
+            println("Auto 2 hat gewonnen!")
+        else
+            println("Es ist ein Unentschieden!")
+    }
 }
 
