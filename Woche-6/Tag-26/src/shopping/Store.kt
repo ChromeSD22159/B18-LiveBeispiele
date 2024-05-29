0054122bae44ca0564d6931a4c4aaca7f9d4663e
GordonLucasSyn <113020395+GordonLucasSyn@users.noreply.github.com>
2024-05-29 10:55:04 +0200

    Tag 26: Vererbung

--- Woche-6/Tag-26/src/shopping/Store.kt
+++ Woche-6/Tag-26/src/shopping/Store.kt
@@ -7,5 +7,22 @@ class Store (
     val speisekarte = mutableListOf<Produkt>()
 
 
+    fun nurGerichte(): List<Gericht>{
+        return speisekarte.filterIsInstance<Gericht>()
+    }
+
+    fun nurVegi(): List<Gericht>{
+        return nurGerichte().filter { it.istVegi }
+    }
+
+    fun nurVegiUndBillig(): List<Gericht>{
+        return nurGerichte().filter { it.istVegi && it.preis < 5.0}
+    }
+
+    fun speiseKarteAusdrucken(){
+        for (artikel in speisekarte){
+            println("${artikel.name} (${artikel::class.simpleName}) für ${artikel.preis} ")
+        }
+    }
 
 }
--- Woche-6/Tag-26/src/shopping/main.kt
+++ Woche-6/Tag-26/src/shopping/main.kt
@@ -9,6 +9,9 @@ fun main(){
         Gericht("Vegane Lasagne", 11.50, true)
     )
 
+    val meinLieblingsGetraenk = Getraenk("Bier", 0.0, false)
+    meinLieblingsGetraenk.istAlk
+
     val pizzaria = Store("Luigi's Pizza", "Luigi")
     pizzaria.speisekarte.add(Getraenk("Cola", 2.0, false))
     pizzaria.speisekarte.add(Getraenk("Bier", 2.5, true))
@@ -16,13 +19,23 @@ fun main(){
     pizzaria.speisekarte.add(Gericht("Pizza Margarita", 8.5, true))
 
     // Produkte nach Preis sortiert
-    pizzaria.speisekarte.sortBy { it.preis }
+    pizzaria.speisekarte.sortBy { produkt ->  produkt.preis }
 
     // Produkte nach Sorte sortiert
     pizzaria.speisekarte.sortBy { it::class.simpleName }
+    println("----")
+    println("Speisekarte nach Sorte sortiert:")
+    println(pizzaria.speisekarte)
+    pizzaria.speiseKarteAusdrucken()
+
+    val pizzaria2 = Store("Mario's Pizza", "Mario")
+    pizzaria2.speisekarte.addAll(pizzaria.speisekarte)
+    pizzaria2.speisekarte.add(Gericht("Pizza Tonno", 8.5, false))
+
 
     // Nur Gerichte ansehen
     val nurGerichte = pizzaria.speisekarte.filterIsInstance<Gericht>()
+
     // Nur vegetarische Gerichte
     val nurVegi = nurGerichte.filter {it.istVegi}
 
